687d6995a667e34525104c37ae8740d6b315709b
LABCAT <shane.watters.nz@gmail.com>
2022-06-13 18:53:00 +1200

    Audio arrangment, repo setup and intial animation expperimentation.

---
+++ src/js/P5SketchWithAudio.js
@@ -0,0 +1,240 @@
+import React, { useRef, useEffect } from "react";
+import "./helpers/Globals";
+import "p5/lib/addons/p5.sound";
+import * as p5 from "p5";
+import { Midi } from '@tonejs/midi'
+import PlayIcon from './functions/PlayIcon.js';
+
+import audio from "../audio/donuts-no-4.ogg";
+import midi from "../audio/donuts-no-4.mid";
+
+const P5SketchWithAudio = () => {
+    const sketchRef = useRef();
+
+    const Sketch = p => {
+
+        p.canvas = null;
+
+        p.canvasWidth = window.innerWidth;
+
+        p.canvasHeight = window.innerHeight;
+
+        p.audioLoaded = false;
+
+        p.player = null;
+
+        p.PPQ = 3840 * 4;
+
+        p.loadMidi = () => {
+            Midi.fromUrl(midi).then(
+                function(result) {
+                    console.log(result);
+                    const noteSet1 = result.tracks[0].notes; 
+                    p.scheduleCueSet(noteSet1, 'executeCueSet1');
+                    p.audioLoaded = true;
+                    document.getElementById("loader").classList.add("loading--complete");
+                    document.getElementById("play-icon").classList.remove("fade-out");
+                }
+            );
+            
+        }
+
+        p.preload = () => {
+            p.song = p.loadSound(audio, p.loadMidi);
+            p.song.onended(p.logCredits);
+        }
+
+        p.scheduleCueSet = (noteSet, callbackName, poly = false)  => {
+            let lastTicks = -1,
+                currentCue = 1;
+            for (let i = 0; i < noteSet.length; i++) {
+                const note = noteSet[i],
+                    { ticks, time } = note;
+                if(ticks !== lastTicks || poly){
+                    note.currentCue = currentCue;
+                    p.song.addCue(time, p[callbackName], note);
+                    lastTicks = ticks;
+                    currentCue++;
+                }
+            }
+        } 
+
+        p.setup = () => {
+            p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
+
+            p.background(0);
+            p.colorMode(p.HSB);
+            p.rectMode(p.CENTER);
+        }
+
+        p.draw = () => {
+            if(p.audioLoaded && p.song.isPlaying()){
+
+            }
+        }
+
+        p.shapeOptions = ['rect', 'ellipse', 'equilateral', 'hexagon', 'octagon'];
+        p.rotationOptions = [3, 4, 6,8,12];
+
+        p.executeCueSet1 = (note) => {
+            console.log(note);
+            const colour = p.color(p.random(0, 360), 100, 100),
+                numOfRotations = p.random(p.rotationOptions),
+                shape = p.random(p.shapeOptions);
+            p.translate(p.width/2,p.height/2); 
+            p.background(0);
+            p.drawDonut(colour, numOfRotations, shape);
+            p.translate(-p.width/2,-p.height/2);
+        }
+
+        p.drawDonut = (colour, numOfRotations, shape) => {
+            //this is where the donut is created
+            p.strokeWeight(0.3);
+            p.noFill();
+            for (var i = 0; i < (numOfRotations * 2); i ++) {
+                //the shape is draw five times with different levels of opacity to create interesting textures
+                for (var j = 0; j <=5; j++) {
+                    p.stroke(colour);
+                    //call the function as detemined by the variable shape
+                    //rect and ellipse are built in p5.js
+                    //tri,hexa & octa are defined in this file
+                    p[shape](0, 20, 400 + (j*3), 400 + (j*3));
+                }
+                p.rotate(p.PI/numOfRotations);
+            }
+        }
+
+        /*
+        * function to draw an equilateral triangle with a set width
+        * based on x, y co-oridinates that are the center of the triangle
+        * @param {Number} x        - x-coordinate that is at the center of triangle
+        * @param {Number} y      	- y-coordinate that is at the center of triangle
+        * @param {Number} width    - radius of the hexagon
+        */
+        p.equilateral = (x, y, width) => {
+            var x1 = x - (width/2);
+            var y1 = y + (width/2);
+            var x2 = x;
+            var y2 = y - (width/2);
+            var x3 = x + (width/2);
+            var y3 = y + (width/2);
+            p.triangle(x1,y1,x2,y2,x3,y3);
+        }
+
+        /*
+        * function to draw a hexagon shape
+        * adapted from: https://p5js.org/examples/form-regular-polygon.html
+        * @param {Number} x        - x-coordinate of the hexagon
+        * @param {Number} y      - y-coordinate of the hexagon
+        * @param {Number} radius   - radius of the hexagon
+        */
+        p.hexagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            var angle = p.TWO_PI / 6;
+            p.beginShape();
+            for (var a = p.TWO_PI/12; a < p.TWO_PI + p.TWO_PI/12; a += angle) {
+                var sx = x + p.cos(a) * radius;
+                var sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
+        }
+
+        /*
+        * function to draw a octagon shape
+        * adapted from: https://p5js.org/examples/form-regular-polygon.html
+        * @param {Number} x        - x-coordinate of the octagon
+        * @param {Number} y      - y-coordinate of the octagon
+        * @param {Number} radius   - radius of the octagon
+        */
+        p.octagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            var angle = p.TWO_PI / 8;
+            p.beginShape();
+            for (var a = p.TWO_PI/16; a < p.TWO_PI + p.TWO_PI/16; a += angle) {
+                var sx = x + p.cos(a) * radius;
+                var sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
+        }
+
+
+        p.mousePressed = () => {
+            if(p.audioLoaded){
+                if (p.song.isPlaying()) {
+                    p.song.pause();
+                } else {
+                    if (parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)) {
+                        p.reset();
+                    }
+                    document.getElementById("play-icon").classList.add("fade-out");
+                    p.canvas.addClass("fade-in");
+                    p.song.play();
+                }
+            }
+        }
+
+        p.creditsLogged = false;
+
+        p.logCredits = () => {
+            if (
+                !p.creditsLogged &&
+                parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
+            ) {
+                p.creditsLogged = true;
+                    console.log(
+                    "Music By: http://labcat.nz/",
+                    "\n",
+                    "Animation By: https://github.com/LABCAT/"
+                );
+                p.song.stop();
+            }
+        };
+
+        p.reset = () => {
+
+        }
+
+        p.updateCanvasDimensions = () => {
+            p.canvasWidth = window.innerWidth;
+            p.canvasHeight = window.innerHeight;
+            p.canvas = p.resizeCanvas(p.canvasWidth, p.canvasHeight);
+        }
+
+        if (window.attachEvent) {
+            window.attachEvent(
+                'onresize',
+                function () {
+                    p.updateCanvasDimensions();
+                }
+            );
+        }
+        else if (window.addEventListener) {
+            window.addEventListener(
+                'resize',
+                function () {
+                    p.updateCanvasDimensions();
+                },
+                true
+            );
+        }
+        else {
+            //The browser does not support Javascript event binding
+        }
+    };
+
+    useEffect(() => {
+        new p5(Sketch, sketchRef.current);
+    }, []);
+
+    return (
+        <div ref={sketchRef}>
+            <PlayIcon />
+        </div>
+    );
+};
+
+export default P5SketchWithAudio;
